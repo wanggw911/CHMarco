7cd559367e254d89f2e4d93fce2637122a74f5d4
Guowen Wang <wangguowen911@foxmail.com>
2017-12-21 17:29:53 +0800

    修改宏配置文件

--- CHMarco/AppMarco+UIKit.h
+++ CHMarco/AppMarco+UIKit.h
@@ -11,29 +11,26 @@
 
 
 
-#define kKeyWindow              [UIApplication sharedApplication].keyWindow
-#define kRootViewController     [[UIApplication sharedApplication].delegate window].rootViewController
-#define kFirstResponder         [kKeyWindow performSelector:@selector(firstResponder)]
+#define kKeyWindow                  [UIApplication sharedApplication].keyWindow
+#define kRootViewController         [[UIApplication sharedApplication].delegate window].rootViewController
+#define kFirstResponder             [kKeyWindow performSelector:@selector(firstResponder)]
 
-#define SINGLE_LINE_WIDTH       (1.f / [UIScreen mainScreen].scale)
+#define kSCREEN_WIDTH               ([UIScreen mainScreen].bounds.size.width)
+#define kSCREEN_HEIGHT              ([UIScreen mainScreen].bounds.size.height)
 
-#define kSCREEN_WIDTH           ([UIScreen mainScreen].bounds.size.width)
-#define kSCREEN_HEIGHT          ([UIScreen mainScreen].bounds.size.height)
+#define kWIDTH_IPHONE               kSCREEN_WIDTH
+#define kHEIGTH_IPHONE              kSCREEN_HEIGHT
 
-#define kWIDTH_IPHONE           kSCREEN_WIDTH
-#define kHEIGTH_IPHONE          kSCREEN_HEIGHT
+#define kIS_Iphone                  (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+#define kIS_Iphone5ORLow            (kSCREEN_WIDTH <= 568)
+#define kIS_IphoneX                 (kSCREEN_WIDTH == 812)
 
-
-#define kIS_IPHONE              (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
-#define kIS_Iphone5ORLow        ([UIScreen mainScreen].bounds.size.height<=568)
-#define kIS_IphoneX             (SCREEN_HEIGHT == 812)
-
-
-#define kView_Of_NIB(x)             [[[NSBundle mainBundle] loadNibNamed:x owner:self options:nil] lastObject]
 #define kNib(x)                     [UINib nibWithNibName:x bundle:nil]
+#define kView_Of_NIB(x)             [[[NSBundle mainBundle] loadNibNamed:x owner:self options:nil] lastObject]
 
 #define kStoryboard_Main            [UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]]
 #define kController_of_MainSB(x)    [kStoryboard_Main instantiateViewControllerWithIdentifier:x]
+
 
 
 
